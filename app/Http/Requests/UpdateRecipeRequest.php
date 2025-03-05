bef6cfcf95d165949d9386286fd88c648958e1cd
chiillguy <elfafaza03@gmail.com>
2025-03-05 11:47:14 +0700

    Create UpdateRecipeRequest.php

---
+++ app/Http/Requests/UpdateRecipeRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRecipeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'chef_id' => 'required|exists:users,id',
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'sometimes|required',
+            'category_id' => 'required|exists:categories,id',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
+        ];
+    }
+}
