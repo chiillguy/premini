1a393b8ed38f4f7030df148d7ab4bdcc36b6f57e
chiillguy <elfafaza03@gmail.com>
2025-03-05 13:38:37 +0700

    Create CookingEventResource.php

---
+++ app/Http/Resources/CookingEventResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CookingEventResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'chef_id' => $this->chef->name,
+            'title' => $this->title,
+            'description' => $this->description,
+            'date' => $this->date,
+            'location' => $this->location,
+            'image' => $this->image,
+        ];
+    }
+}
