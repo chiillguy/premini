800fea5904b5394dc3022040e271a26e182f2a53
chiillguy <elfafaza03@gmail.com>
2025-03-05 09:45:32 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\RecipeIngredientController;
 use App\Http\Controllers\RecipeReviewController;
 use App\Http\Controllers\RecipeStepController;
 use App\Http\Middleware\RoleMiddleware;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register'])->name('register');
@@ -17,24 +16,25 @@ Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('me', [AuthController::class, 'me'])->name('me');
-    Route::apiResource('recipe-reviews', RecipeReviewController::class);
 
+    // **User, Chef, Admin bisa melihat resep & memberikan review**
+    Route::get('recipes', [RecipeController::class, 'index'])->name('recipes.index');
+    Route::apiResource('recipe-reviews', RecipeReviewController::class)->only(['index', 'store', 'show']);
+
+    // **Admin**
     Route::middleware(RoleMiddleware::class . ':admin')->group(function () {
         Route::apiResource('categories', CategoryController::class);
     });
-    
+
+    // **Chef & Admin**
     Route::middleware(RoleMiddleware::class . ':admin,chef')->group(function () {
-        Route::apiResource('recipes', RecipeController::class);
-        Route::apiResource('cooking-event', CookingEventController::class);
+        Route::apiResource('recipes', RecipeController::class)->except(['index']);
+        Route::apiResource('cooking-events', CookingEventController::class);
     });
-    
+
+    // **Chef**
     Route::middleware(RoleMiddleware::class . ':chef')->group(function () {
         Route::apiResource('recipe-steps', RecipeStepController::class);
         Route::apiResource('recipe-ingredients', RecipeIngredientController::class);
-    });
-
-    Route::middleware(RoleMiddleware::class . ':admin,chef,user')->group(function () {
-        Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes');
-
     });
 });
