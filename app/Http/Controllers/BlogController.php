5e19f32e9f85cd6c273858a0b945eb7d599fd998
chiillguy <elfafaza03@gmail.com>
2025-03-09 01:48:35 +0700

    create

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BlogRequest;
+use App\Http\Requests\UpdateBlogRequest;
+use App\Http\Resources\BlogResource;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return BlogResource::collection(Blog::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(BlogRequest $request)
+    {
+        $blog = Blog::create($request->validated());
+
+        return response([
+            'message' => 'Blog created',
+            'blog' => new BlogResource($blog)
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        return new BlogResource($blog);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBlogRequest $request, Blog $blog)
+    {
+        $blog->update($request->validated());
+
+        return response()->json([
+            'message' => 'Blog updated',
+            'blog' => new BlogResource($blog)
+        ], 202);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+
+        return response()->json(['message' => 'Blog deleted successfully'], 200);
+    }
+}
