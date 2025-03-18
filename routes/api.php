9052f2ae4e0829ffe96c32a1c6c8e458351c577b
chiillguy <lovelywnmer@gmail.com>
2025-03-18 08:34:45 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,35 +12,38 @@ use App\Http\Controllers\RecipeStepController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register'])->name('register');
-Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('me', [AuthController::class, 'me'])->name('me');
-
-    // **User, Chef, Admin bisa melihat resep & memberikan review**
-    Route::apiResource('recipes', RecipeController::class)->only(['index', 'show']);
-    Route::apiResource('recipe-reviews', RecipeReviewController::class)->only(['index', 'store', 'show', 'update']);
-    Route::apiResource('cooking-events', CookingEventController::class)->only(['index', 'show']);
-
-    // **Admin**
-    Route::middleware(RoleMiddleware::class . ':admin')->group(function () {
-        Route::apiResource('categories', CategoryController::class);
-        Route::apiResource('recipe-reviews', RecipeReviewController::class)->only('delete');
-        Route::apiResource('blogs', BlogController::class);
-        Route::apiResource('blog-contents', BlogContentController::class);
-    });
-
-    // **Chef & Admin**
-    Route::middleware(RoleMiddleware::class . ':admin,chef')->group(function () {
-        Route::apiResource('cooking-events', CookingEventController::class)->except(['index', 'show']);
-    });
+Route::middleware(['cors'])->group(function(){
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
     
-    // **Chef**
-    Route::middleware(RoleMiddleware::class . ':chef')->group(function () {
-        Route::apiResource('recipes', RecipeController::class)->except(['index', 'show']);
-        Route::apiResource('recipe-steps', RecipeStepController::class);
-        Route::apiResource('recipe-ingredients', RecipeIngredientController::class);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('me', [AuthController::class, 'me'])->name('me');
+    
+        // **User, Chef, Admin bisa melihat resep & memberikan review**
+        Route::apiResource('recipes', RecipeController::class)->only(['index', 'show']);
+        Route::apiResource('recipe-reviews', RecipeReviewController::class)->only(['index', 'store', 'show', 'update']);
+        Route::apiResource('cooking-events', CookingEventController::class)->only(['index', 'show']);
+    
+        // **Admin**
+        Route::middleware(RoleMiddleware::class . ':admin')->group(function () {
+            Route::apiResource('categories', CategoryController::class);
+            Route::apiResource('recipe-reviews', RecipeReviewController::class)->only('delete');
+            Route::apiResource('blogs', BlogController::class);
+            Route::apiResource('blog-contents', BlogContentController::class);
+        });
+    
+        // **Chef & Admin**
+        Route::middleware(RoleMiddleware::class . ':admin,chef')->group(function () {
+            Route::apiResource('cooking-events', CookingEventController::class)->except(['index', 'show']);
+        });
+        
+        // **Chef**
+        Route::middleware(RoleMiddleware::class . ':chef')->group(function () {
+            Route::apiResource('recipes', RecipeController::class)->except(['index', 'show']);
+            Route::apiResource('recipe-steps', RecipeStepController::class);
+            Route::apiResource('recipe-ingredients', RecipeIngredientController::class);
+        });
     });
 });
